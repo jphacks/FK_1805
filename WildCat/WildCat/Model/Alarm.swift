ce9149ac181cfadc7b273352f9bb8bfcb74ce221
Azuma Sato <lifeghtcloerce5291@gmail.com>
2018-10-23 23:31:57 +0900

    add Model

---
+++ WildCat/WildCat/Model/Alarm.swift
@@ -0,0 +1,15 @@
+//
+//  Alarm.swift
+//  WildCat
+//
+//  Created by Azuma on 2018/10/23.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import Foundation
+
+class Alarm {
+    @objc dynamic var id: Int?
+    @objc dynamic var date: Date?
+    @objc dynamic var pattern: Pattern?
+}
---
+++ WildCat/WildCat/Model/Pattern.swift
@@ -0,0 +1,15 @@
+//
+//  Pattern.swift
+//  WildCat
+//
+//  Created by Azuma on 2018/10/23.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import Foundation
+
+class Pattern {
+    @objc dynamic var id: Int?
+    @objc dynamic var description: String?
+    @objc dynamic var imagePath: String?
+}
