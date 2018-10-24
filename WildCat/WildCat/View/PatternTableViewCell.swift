af2398e3b63ecfab1981fbf256e168399b953b5a
KO-YAMAKO <ko.foot1103@gmail.com>
2018-10-24 14:40:14 +0900

    add new class PatternTableViewCell

---
+++ WildCat/WildCat/View/PatternTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PatternTableViewCell.swift
+//  WildCat
+//
+//  Created by 山浦功 on 2018/10/24.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import UIKit
+
+class PatternTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
