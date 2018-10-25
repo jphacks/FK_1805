6a162469ef3b1dfe5cd9aa085688f758dc461703
Kageken <kageken@KagekennoMacBook-Pro.local>
2018-10-25 14:55:23 +0900

    Add AlarmTableViewCell.swift

---
+++ WildCat/WildCat/View/AlarmTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AlarmTableViewCell.swift
+//  WildCat
+//
+//  Created by 陰山賢太 on 2018/10/25.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import UIKit
+
+class AlarmTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
