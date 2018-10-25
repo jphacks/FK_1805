d1f083d2822b87ad5a7424f758a300111b48a218
Azuma Sato <lifeghtcloerce5291@gmail.com>
2018-10-25 13:18:47 +0900

    modelの調整

--- WildCat/WildCat/Model/Alarm.swift
+++ WildCat/WildCat/Model/Alarm.swift
@@ -14,3 +14,45 @@ class Alarm: Object {
     @objc dynamic var date: Date = Date()
     @objc dynamic var pattern: Pattern?
 }
+
+extension Alarm {
+    class func add(alarm: Alarm) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(alarm)
+            }
+        } catch {
+            print(error)
+        }
+    }
+
+    class func update(alarm: Alarm, block: (Results<Alarm>) -> Void) {
+        let realm = try! Realm()
+        add(alarm: alarm)
+        block(realm.objects(Alarm.self))
+    }
+
+    class func delete(alarm: Alarm) {
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.delete(alarm)
+            }
+        } catch {
+            print(error)
+        }
+    }
+
+    class func deleteAll() {
+        do {
+            let realm = try Realm()
+            let allAlarm = realm.objects(Alarm.self)
+            try realm.write {
+                realm.delete(allAlarm)
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
