1c89c9f75b58479cddc8120c5b41b6ba85ca7e29
Kageken <kageken@KagekennoMacBook-Pro.local>
2018-10-26 20:20:39 +0900

    Edit AlarmManager

--- WildCat/WildCat/Common/AlarmManager.swift
+++ WildCat/WildCat/Common/AlarmManager.swift
@@ -8,3 +8,41 @@
 
 import Foundation
 import UserNotifications
+
+class AlarmManager {
+    class func allowNotification() -> Void {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if error != nil {
+                return
+            }
+
+            if granted {
+                print("通知許可")
+            } else {
+                print("通知拒否")
+            }
+        }
+    }
+
+    class func setAlarm(date: Date, pattern: Pattern) -> Void {
+        // 通知する時間をセット
+        var notificationTime = DateComponents()
+        notificationTime.hour = 12
+        notificationTime.minute = 0
+        let trigger = UNCalendarNotificationTrigger(dateMatching: notificationTime, repeats: false)
+
+        // 通知する内容をセット
+        let content = UNMutableNotificationContent()
+        content.title = "Title"
+        content.body = pattern.message
+        content.sound = UNNotificationSound.default
+        content.attachments = [try! UNNotificationAttachment(identifier: "id", url: URL(fileReferenceLiteralResourceName: pattern.imagePath), options: nil)]
+
+        // 通知リクエストを作成
+        let request = UNNotificationRequest(identifier: "id", content: content, trigger: trigger)
+
+        // 通知セット
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+}
