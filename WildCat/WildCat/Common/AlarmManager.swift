721b99ff3c2bc122c5d04273cfa8be9b46bf343a
Kageken <kageken@KagekennoMacBook-Pro.local>
2018-10-26 10:48:34 +0900

    Add notification file

---
+++ WildCat/WildCat/Common/AlarmManager.swift
@@ -0,0 +1,10 @@
+//
+//  AlarmManager.swift
+//  WildCat
+//
+//  Created by 陰山賢太 on 2018/10/25.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
--- WildCat/WildCat/Controller/Alarm/AlarmTableViewController.swift
+++ WildCat/WildCat/Controller/Alarm/AlarmTableViewController.swift
@@ -16,12 +16,6 @@ class AlarmTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
