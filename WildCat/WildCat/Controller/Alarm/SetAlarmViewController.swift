b2754dcbb632549b8e0fc37cd60f8174ae234692
Kageken <kageken@KagekennoMacBook-Pro.local>
2018-10-27 10:31:59 +0900

    Edit alarmManager

---
+++ WildCat/WildCat/Controller/Alarm/SetAlarmViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SetAlarmViewController.swift
+//  WildCat
+//
+//  Created by 陰山賢太 on 2018/10/27.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import UIKit
+
+class SetAlarmViewController: UIViewController {
+
+    // MARK: - Propaties
+    
+    @IBOutlet weak var timePicker: UIDatePicker!
+    @IBOutlet weak var saveButton: UIButton!
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Actions of Buttons
+
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+        return
+    }
+}
