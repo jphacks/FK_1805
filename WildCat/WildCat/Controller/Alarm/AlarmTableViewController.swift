17cd1c429ee77dbd7d6323ecf23fedc6a4995d4c
Kageken <kageken@KagekennoMacBook-Pro.local>
2018-10-27 15:41:44 +0900

    Fix

--- WildCat/WildCat/Controller/Alarm/AlarmTableViewController.swift
+++ WildCat/WildCat/Controller/Alarm/AlarmTableViewController.swift
@@ -53,12 +53,10 @@ class AlarmTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            //let cell = tableView.dequeueReusableCell(withIdentifier: "alarmCell", for: indexPath) as! AlarmTableViewCell
             let targetAlarm = self.alarms[indexPath.item]
             Alarm.delete(alarm: targetAlarm)
             self.alarms.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-            //cell.update(target: targetAlarm)
             return
         }
     }
