b2c81180575c90f8933783c242855d86b5f84dd0
Kageken <kageken@KagekennoMacBook-Pro.local>
2018-10-27 19:41:22 +0900

    Modified Design and View Refresh

--- WildCat/WildCat/Controller/Alarm/AlarmTableViewController.swift
+++ WildCat/WildCat/Controller/Alarm/AlarmTableViewController.swift
@@ -21,6 +21,10 @@ class AlarmTableViewController: UITableViewController {
         refreshControl?.addTarget(self, action: #selector(self.refreshData(sender:)), for: .valueChanged)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        self.setup()
+    }
+
     private func setup() {
         let data = Array(Alarm.read())
         self.alarms = data
